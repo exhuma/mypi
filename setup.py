67d5429bb5175a9231ad8190c23587c4a5c924cd
Michel Albert <michel@albert.lu>
2013-12-06 14:19:25 +0100

    PEP8 fixes.

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 from setuptools import setup, find_packages
-from setuptools.command.install import install
 from os.path import join
 from sys import prefix
-import os
 
 PACKAGE = "mypi"
 NAME = "mypi"
@@ -20,16 +18,15 @@ setup(
     author_email=AUTHOR_EMAIL,
     license="GPL",
     include_package_data=True,
-    install_requires = [
-      'Flask',
-      'alembic',
-      'SQLAlchemy',
-      'config_resolver >=4.0, <5.0'
-      ],
+    install_requires=[
+        'Flask',
+        'alembic',
+        'SQLAlchemy',
+        'config_resolver >=4.0, <5.0'
+    ],
     packages=find_packages(exclude=["tests.*", "tests"]),
     data_files=[
-        (join(prefix,'usr/share/docs/mypi/examples'),
-        ['mod_wsgi/app.wsgi', 'mod_wsgi/example.apache.conf']),
-        ],
+        (join(prefix, 'usr/share/docs/mypi/examples'),
+         ['mod_wsgi/app.wsgi', 'mod_wsgi/example.apache.conf']),
+    ],
 )
-
