9c676855a91b9988511de98e82306931ec02c672
Michel Albert <michel@albert.lu>
2013-12-07 12:45:28 +0100

    Example test script added.

---
+++ mypi/test/test_http.py
@@ -0,0 +1,19 @@
+import os
+import unittest
+import tempfile
+
+
+class FlaskrTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
+        flaskr.app.config['TESTING'] = True
+        self.app = flaskr.app.test_client()
+        flaskr.init_db()
+
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(flaskr.app.config['DATABASE'])
+
+if __name__ == '__main__':
+    unittest.main()
