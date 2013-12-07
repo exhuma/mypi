eff21657ce3d074d30e50791264bd3843c0f0e79
Michel Albert <michel@albert.lu>
2013-12-07 12:49:59 +0100

    I like bold white more...

--- fabfile.py
+++ fabfile.py
@@ -21,13 +21,15 @@ def develop():
     """
     fab.local('[ -d .env ] || virtualenv .env')
     fab.local('mkdir -p .exhuma/mypi')
-    db_url = fab.prompt(clr.green('Database URL:'), default='sqlite:///dev.db')
+    db_url = fab.prompt(clr.white('Database URL:', bold=True),
+                        default='sqlite:///dev.db')
     dev_conf_name = '.exhuma/mypi/app.ini'
     with open(dev_conf_name, 'w') as fp:
         fp.write(CONFIG_TEMPLATE.format(db_url=db_url))
-        print(clr.green('Development config saved to ') +
-              clr.yellow(dev_conf_name))
-    print(clr.green('Installing project into development environment...'))
+        print(clr.white('Development config saved to ', bold=True) +
+              clr.green(dev_conf_name))
+    print(clr.white('Installing project into development environment...',
+                    bold=True))
     fab.local('./.env/bin/pip install -e .')
     fab.execute(upgrade_local_db)
 
@@ -37,5 +39,5 @@ def upgrade_local_db():
     """
     Upgrades the development database to the latest alembic revision
     """
-    print(clr.green('Upgrading database...'))
+    print(clr.white('Upgrading database...', bold=True))
     fab.local('./.env/bin/alembic upgrade head')
